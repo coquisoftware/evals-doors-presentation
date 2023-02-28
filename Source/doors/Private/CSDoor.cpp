bfd111bf6d98e92cef9fc3c632b24acc768d3b35
Jason Viterna <jason@coquisoftware.com>
2023-02-28 17:19:32 -0400

    Building door primitive

---
+++ Source/doors/Private/CSDoor.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "doors/Public/CSDoor.h"
+
+
+// Sets default values
+ACSDoor::ACSDoor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACSDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACSDoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
